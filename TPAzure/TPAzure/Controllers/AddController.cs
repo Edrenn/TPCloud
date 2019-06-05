626625b6d6be2fd45cb976e96ceef0b887155034
Edrenn <edern.rodriguez@orange.fr>
2019-06-05 10:27:44 +0200

    Change form

---
+++ TPAzure/TPAzure/Controllers/AddController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TPAzure.Models;
+
+namespace TPAzure.Controllers
+{
+    public class AddController : Controller
+    {
+        // GET: Add
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Do somthing
+        }
+
+        public void Add(Advert newAdvert)
+        {
+            // Add advert
+        }
+    }
+}
---
+++ TPAzure/TPAzure/Models/Advert.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPAzure.Models
+{
+    public class Advert
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string ImgPath { get; set; }
+    }
+}
